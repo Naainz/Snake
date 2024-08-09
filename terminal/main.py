209cf1ac37572eddf1c0a1a9b7ae112df9a53ead
Stefan <67121265+Naainz@users.noreply.github.com>
2024-08-09 12:24:09 +0300

    Python first try

--- terminal/main.py
+++ terminal/main.py
@@ -1,103 +1,170 @@
 import curses
 import time
-import random
+import sys
+from random import randint
 
-GRID_WIDTH = 30
-GRID_HEIGHT = 10
-SNAKE_CHAR = '🐍'
-FOOD_CHAR = '🍎'
-EMPTY_CHAR = ' '
-DELAY = 0.2
+class Field:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.icons = {
+            0: """.""",  
+            1: """🟩""",  
+            2: """🟢""",  
+            3: """🍎""",  
+        }
+        self.snake_coords = []
+        self._generate_field()
+        self.add_entity()
 
-def initialize_game():
-    snake = [(5, 5), (5, 6), (5, 7)]  
-    direction = (0, 1)  
-    food = generate_food(snake)
-    game_over = False
-    return snake, direction, food, game_over
+    def add_entity(self):
+        while True:
+            i = randint(0, self.height - 1)
+            j = randint(0, self.width - 1)
+            entity = [i, j]
+            if entity not in self.snake_coords:
+                self.field[i][j] = 3
+                break
 
-def generate_food(snake):
-    while True:
-        food = (random.randint(0, GRID_HEIGHT - 1), random.randint(0, GRID_WIDTH - 1))
-        if food not in snake:
-            return food
+    def _generate_field(self):
+        self.field = [[0 for j in range(self.width)] for i in range(self.height)]
 
-def draw_game(snake, food, stdscr):
-    stdscr.clear()
-    grid = [[EMPTY_CHAR] * GRID_WIDTH for _ in range(GRID_HEIGHT)]
+    def _clear_field(self):
+        self.field = [[j if j != 1 and j != 2 else 0 for j in i] for i in self.field]
 
-    for segment in snake:
-        grid[segment[0]][segment[1]] = SNAKE_CHAR
+    def render(self, screen):
+        self._clear_field()
 
-    grid[food[0]][food[1]] = FOOD_CHAR
+        for i, j in self.snake_coords:
+            self.field[i][j] = 1
 
-    for row in grid:
-        stdscr.addstr(''.join(row) + '\n')
-    
-    stdscr.refresh()
+        head = self.snake_coords[-1]
+        self.field[head[0]][head[1]] = 2
 
-def update_game(snake, direction, food):
-    new_head = (snake[0][0] + direction[0], snake[0][1] + direction[1])
+        for i in range(self.height):
+            row = ''
+            for j in range(self.width):
+                row += self.icons[self.field[i][j]]
+            
+            
+            screen.addstr(i, 0, row)
 
-    if (new_head[0] < 0 or new_head[0] >= GRID_HEIGHT or
-        new_head[1] < 0 or new_head[1] >= GRID_WIDTH or
-        new_head in snake):
-        return snake, food, True
+    def get_entity_pos(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                if self.field[i][j] == 3:
+                    return [i, j]
+        return [-1, -1]
 
-    snake.insert(0, new_head)
+    def is_snake_eat_entity(self):
+        entity = self.get_entity_pos()
+        head = self.snake_coords[-1]
+        return entity == head
 
-    if new_head == food:
-        food = generate_food(snake)
-    else:
-        snake.pop()
+class Snake:
+    def __init__(self, name):
+        self.name = name
+        self.direction = curses.KEY_RIGHT
+        self.coords = [[0, 0], [0, 1], [0, 2], [0, 3]]
 
-    return snake, food, False
+    def set_direction(self, ch):
+        if ch == curses.KEY_LEFT and self.direction == curses.KEY_RIGHT:
+            return
+        if ch == curses.KEY_RIGHT and self.direction == curses.KEY_LEFT:
+            return
+        if ch == curses.KEY_UP and self.direction == curses.KEY_DOWN:
+            return
+        if ch == curses.KEY_DOWN and self.direction == curses.KEY_UP:
+            return
+        self.direction = ch
 
-def start_game_countdown(stdscr):
-    for i in range(3, 0, -1):
-        stdscr.clear()
-        stdscr.addstr(0, 0, f"Game will start in {i} seconds...")
-        stdscr.refresh()
-        time.sleep(1)
+    def level_up(self):
+        a = self.coords[0]
+        b = self.coords[1]
+        tail = a[:]
 
-def play_game(stdscr):
-    curses.curs_set(0)
-    stdscr.nodelay(1)
+        if a[0] < b[0]:
+            tail[0] -= 1
+        elif a[1] < b[1]:
+            tail[1] -= 1
+        elif a[0] > b[0]:
+            tail[0] += 1
+        elif a[1] > b[1]:
+            tail[1] += 1
 
-    start_game_countdown(stdscr)
-    snake, direction, food, game_over = initialize_game()
+        self.coords.insert(0, tail)
 
-    while not game_over:
-        draw_game(snake, food, stdscr)
+    def is_alive(self):
+        head = self.coords[-1]
+        snake_body = self.coords[:-1]
 
-        key = stdscr.getch()
-        if key == curses.KEY_UP and direction != (1, 0):
-            direction = (-1, 0)
-        elif key == curses.KEY_DOWN and direction != (-1, 0):
-            direction = (1, 0)
-        elif key == curses.KEY_LEFT and direction != (0, 1):
-            direction = (0, -1)
-        elif key == curses.KEY_RIGHT and direction != (0, -1):
-            direction = (0, 1)
+        if head[0] < 0 or head[0] >= self.field.height or head[1] < 0 or head[1] >= self.field.width:
+            print("Game Over! You hit a wall.")
+            return False
+        if head in snake_body:
+            print("Game Over! You ran into yourself.")
+            return False
+        return True
 
-        snake, food, game_over = update_game(snake, direction, food)
-        time.sleep(DELAY)
+    def move(self):
+        head = self.coords[-1][:]
 
-    draw_game(snake, food, stdscr)
-    stdscr.addstr(11, 0, "Game Over! Press Enter to play again or 'q' to quit.")
-    stdscr.refresh()
+        if self.direction == curses.KEY_UP:
+            head[0] -= 1
+        elif self.direction == curses.KEY_DOWN:
+            head[0] += 1
+        elif self.direction == curses.KEY_RIGHT:
+            head[1] += 1
+        elif self.direction == curses.KEY_LEFT:
+            head[1] -= 1
 
-    while True:
-        key = stdscr.getch()
-        if key == ord('\n') or key == 10:
-            return True
-        elif key == ord('q'):
+        del self.coords[0]
+        self.coords.append(head)
+        self.field.snake_coords = self.coords
+
+        if not self.is_alive():
             return False
 
-def main(stdscr):
-    while True:
-        if not play_game(stdscr):
-            break
+        if self.field.is_snake_eat_entity():
+            curses.beep()
+            self.level_up()
+            self.field.add_entity()
 
-if __name__ == "__main__":
+        return True
+
+    def set_field(self, field):
+        self.field = field
+
+def main(screen):
+    while True:
+        screen.clear()
+        field = Field(30, 20)
+        snake = Snake("Hackclub Arcade")
+        snake.set_field(field)
+        screen.timeout(100)
+
+        while True:
+            ch = screen.getch()
+            if ch != -1:
+                snake.set_direction(ch)
+
+            if ch == ord('q'):
+                sys.exit()
+
+            if not snake.move():
+                break
+
+            field.render(screen)
+            screen.refresh()
+            time.sleep(0.1)
+
+        screen.addstr(0, 0, "Press Enter to play again or 'q' to quit.")
+        screen.refresh()
+        key = screen.getch()
+        if key == ord('q'):
+            sys.exit()
+        elif key == 10:  
+            continue
+
+if __name__ == '__main__':
     curses.wrapper(main)
